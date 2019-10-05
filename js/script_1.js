f5e7e3e1be89460d865c1940c189041e56777786
MasterLex <sashatv138@gmail.com>
2019-10-05 11:33:48 +0300

    My first commit

---
+++ js/script_1.js
@@ -0,0 +1,90 @@
+"use strict";
+
+//  a = 5;
+// console.log(a);
+ // First ans Second lessons
+
+
+//  console.log(leftBorderWidth);
+ //  console.log(second);
+
+ var leftBorderWidth = 1; {
+     let second = 2;
+    //  console.log(second);
+ }
+
+ const pi = 3.14;
+
+//  console.log(leftBorderWidth);
+ //  console.log(second);
+
+ // var infin = 4/0; // infinity
+ //  var number = 5;
+ //  var string = "Fuck off!";
+ //  var sym = Symbol();
+ //  var boolean = true;
+ //  var b = "";
+ //  var a; //undefined;
+ //  var obj = {};
+ //  var array = [1,2,3];
+
+ let person = {
+     name: "Master",
+     lastName: "Lex",
+     age: 54,
+     isMarried: true
+ };
+
+//  console.log(person.lastName);
+//  console.log(person["name"]);
+
+ let arr = ["plum.png","butt.png","legs.png"];
+//  console.log(arr[2]);
+
+ let imgArr = ['../img/hd-space.jpg','../img/earth-space.jpg'];
+
+//  console.log(typeof (imgArr));
+
+//  alert("fuck off!");
+
+//  let answer = confirm("Who the fuck are you?");
+//  console.log(answer);
+
+//  let answer = +prompt("Are you nuts?", "Sure");
+//  console.log(typeof (answer));
+
+
+// ********************************
+// INCREMENT And Dicrement
+ 
+//  let x = "4";
+//  console.log(typeof x);
+//  console.log(typeof +x);
+
+ let incr = 10,
+     decr = 10;
+
+//  incr++;
+//  decr--;
+//  console.log(incr, decr);
+
+//  console.log(++incr,--decr);
+ console.log(incr++, decr--);
+ console.log(incr,decr);
+
+ console.log(5%2);
+ console.log("2" == 2);
+ console.log("2" === 2);
+
+ let isChecked = false,
+     isClose = false;
+
+ console.log(isChecked && isClose);
+ console.log(isChecked || isClose);
+ console.log(isChecked || !isClose);
+
+
+
+
+
+
